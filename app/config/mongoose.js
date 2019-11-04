7e06bc911205a826677710602decf78a2264f90f
Robert Rojas Fernandez <rrojas@brangerbriz.com>
2019-11-04 12:21:11 -0500

    testing uri connection for mongodb atlas

--- app/config/mongoose.js
+++ app/config/mongoose.js
@@ -3,9 +3,18 @@ const mongoose = require("mongoose")
 
 exports.connect = () => {
   const mongoDB = process.env.MONGO_DB || "mongodb://mongo/userManager"
+  const uri = 'mongodb://userManager:YWHFQyLkMl2Pl6kM@' +
+  'testmongo0-shard-00-00-zvbus.mongodb.net:27017,' +
+  'testmongo0-shard-00-01-zvbus.mongodb.net:27017,' +
+  'testmongo0-shard-00-02-zvbus.mongodb.net:27017/userManager?' +
+  'ssl=true&replicaSet=testMongo0-shard-0&authSource=admin'
   const debug = process.env.MONGO_DEBUG || true
 
-  mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'userManager' }).then(() => {
+  console.log('Connection!')}).catch(err => {
+  console.log(uri)
+  console.error(err)
+  })
 
   mongoose.Promise = global.Promise
   mongoose.set("debug", debug)
